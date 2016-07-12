d602aea7d5e0fa5328ce2ec2438e95b2d7a20b09
subinmath <subin.mathew@crichq.com>
2016-07-12 11:56:45 +0530

    Update install.sh
    updated the spelling of easy_install3 boto3 python-dateutil which was causing error

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt-get update
 apt-get install -y python3 python3-setuptools python-pip
 pip install awscli --ignore-installed six
-easy_install3 boto3 pyhton-dateutil
+easy_install3 boto3 python-dateutil
 python3 setup.py install
